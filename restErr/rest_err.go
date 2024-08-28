a911220419e313303fea7f7196a22ed13455af99
Amandaakareen <amandaakareenn@gmail.com>
2024-08-28 19:47:25 -0300

    criando restErr

---
+++ restErr/rest_err.go
@@ -0,0 +1,55 @@
+package resterr
+
+import "net/http"
+
+type RestErr struct {
+	Message string `json:"message"`
+	Err     string `json:"error"`
+	Code    int64  `json:"code"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message, err string, code int64) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+	}
+
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad request",
+		Code:    http.StatusBadRequest,
+	}
+
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Internal_Server_Error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Not_Found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
